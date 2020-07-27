72e8b02224a85094a101db8430dedf538527bd1f
cghbh <1670755868@qq.com>
2020-07-27 15:33:41 +0800

    docs modify again

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -51,7 +51,9 @@ module.exports = {
 					{title: 'Input 输入框', path:'/input/'},
 					{title: 'InputNumber 计数器', path:'/input-number/'},
 					{title: 'Form表单', path: '/form/' },
-					{title: 'Upload上传', path: '/upload/'}
+					{title: 'Upload上传', path: '/upload/'},
+					{title: 'Tree 树形控件', path: '/tree/'},
+					{title: 'Table 表格', path: '/table/'}
 				]
 			},
 			{
@@ -62,6 +64,20 @@ module.exports = {
 					{title: 'Message 消息提示', path:'/message/'},
 					{title: 'MessageBox 弹框', path:'/message-box/'},
 					{title: 'Modal 复杂弹框', path:'/modal/'}
+				]
+			},
+			{
+				title: '其他组件',
+				collapsable: false,
+				sidebarDepth: 0,
+				children: [
+					{title: 'Calendar 日历', path:'/calendar/'},
+					{title: 'Swiper 轮播图', path:'/swiper/'},
+					{title: 'Pagination 分页', path:'/pagination/'},
+					{title: 'Badge 标记', path:'/badge/'},
+					{title: 'Tabs 标签页', path:'/tabs/'},
+					{title: 'Collapse 折叠面板', path:'/collapse/'},
+					{title: 'Select 选择器', path:'/select/'}
 				]
 			}
 		],
