8e5cdd45cbaaf9af6124b3ead2f1fa219487551c
cghbh <1670755868@qq.com>
2020-08-28 19:16:22 +0800

    完成Spread卷轴组件的说明文档

---
+++ docs/docs/.vuepress/components/spread/exam.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<!-- <m-switch v-model="value"></m-switch> -->
+		<m-button :type="visible1 ? 'primary': 'success'" @click="visible1 = !visible1">{{ visible1 ? '纵向收起': '纵向展开'}}</m-button>
+		<div class="spread-example-divide"></div>
+		<m-spread :visible="visible1">
+			<div style="width: 500px; height: 240px;background-color: skyblue;"></div>
+		</m-spread>
+		
+		<!-- 横向	 -->
+		<div class="spread-example-divide"></div>
+		<m-button :type="visible2 ? 'primary': 'success'" @click="visible2 = !visible2">{{ visible2 ? '横向收起': '横向展开'}}</m-button>
+		<div class="spread-example-divide"></div>
+		<m-spread horizontal :visible="visible2">
+			<div style="width: 500px; height: 240px;background-color: skyblue;"></div>
+		</m-spread>
+		
+	</div>
+</template>
+
+<script>
+import MSpread from '../../../../../packages/components/spread/index.js'
+import MButton from '../../../../../packages/components/button/index.js'
+export default {
+	data() {
+		return {
+			visible1: true,
+			visible2: true
+		}
+	},
+	components: {
+		MSpread,
+		MButton
+	}
+}
+</script>
+
+<style scoped>
+.spread-example-divide {
+	margin: 25px 0;
+}
+</style>
--- docs/docs/spread/README.md
+++ docs/docs/spread/README.md
@@ -1,3 +1,44 @@
 ## Spread卷轴组件
 
-#### 具体API尚在思考中，文档说明随后发布
+#### 基础用法
+用于提供水平和垂直方向展开收起动画功能的小组件。
+
+<base-code>
+
+ <template slot="display">
+
+ <spread-exam></spread-exam>
+
+ </template>
+
+ <template>
+
+ ```vue
+ <m-spread :visible="visible1">
+ 	<div style="width: 500px; height: 240px;background-color: skyblue;"></div>
+ </m-spread>
+
+ <m-spread horizontal :visible="visible2">
+ 	<div style="width: 500px; height: 240px;background-color: skyblue;"></div>
+ </m-spread>
+ 
+ <script>
+ export default {
+ 	data() {
+ 		return {
+ 			visible1: true,
+ 			visible2: true
+ 		}
+ 	}
+ }
+ </script>
+ 
+ 
+ 
+ ```
+
+ </template>
+
+ </base-code>
+
+
